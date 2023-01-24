9b248894356523f1f13976ac00bf6d4f3c01e06a
JSamartino <87385582+JSamartino@users.noreply.github.com>
2023-01-24 09:34:23 -0300

    Commit

---
+++ db/conn.js
@@ -0,0 +1,14 @@
+const mysql=require("mysql")
+
+const pool = mysql.createPool({
+    connectionLimit:10,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodemysql2'
+
+
+
+})
+
+module.exports = pool
---
+++ index.js
@@ -0,0 +1,114 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const mysql = require('mysql')
+const conn =require("./db/conn")
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.post('/books/insertbook', function (req, res) {
+  const title = req.body.title
+  const pageqty = req.body.pageqty
+
+  //const query = "INSERT INTO books (title, pageqty) VALUES "+title+" "+pageqty //${pageqty}
+  const query = `INSERT INTO books (title, pageqty) VALUES ('${title}', ${pageqty})`
+  conn.query(query, function (err) {
+    if (err) {
+      console.log(err)
+    }
+
+    res.redirect('/')
+  })
+})
+app.get("/books", (req,res)=>{
+  const sql ="SELECT * FROM books"
+  
+  conn.query(sql,function(err,data){
+    if (err) {
+      console.log(err)
+      return
+    }
+    const books =data
+    console.log(books)
+    res.render("books", {books})
+  })
+
+})
+app.get("/books/:id",(req,res)=>{
+  const id = req.params.id
+
+  const sql = "SELECT * FROM books where id="+id
+  conn.query(sql , function(err,data){
+    if(err){
+      console.log(err)
+      return
+    }
+    const book =data[0]
+    res.render("book",{book})
+
+
+  })
+
+})
+app.get("/books/edit/:id",(req,res)=>{
+
+  const id = req.params.id
+  const sql = "SELECT * FROM books WHERE id= "+id
+  conn.query(sql , function(err,data){
+    if(err){
+      console.log(err)
+      return
+    }
+    const book =data[0]
+    res.render("editbook",{book})
+  })
+
+})
+app.post("/books/updatebook",(req,res)=>{
+  const id= req.body.id
+  const title = req.body.title
+  const pageqty=req.body.pageqty
+
+  const sql = `UPDATE books SET title = '${title}', pageqty = ${pageqty} WHERE id = ${id}`
+  conn.query(sql,function(err){
+    if(err){
+      console.log(err)
+      return
+    }
+    res.redirect("/books")
+  })
+
+})
+app.post("/books/remove/:id",(req,res)=>{
+
+  const id =req.params.id
+  const sql= "DELETE FROM books WHERE id = "+id
+
+  conn.query(sql,function(err){
+    if(err){
+      console.log(err)
+      return
+    }
+    res.redirect("/books")
+
+  })
+
+})
+
+
+app.listen(3000)
